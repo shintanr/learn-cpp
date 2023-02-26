d1fd0e8fa56b3eb406cd2b53a188eb3c538de576
shintanr <shintanurrohmah143@gmail.com>
2023-02-26 08:53:30 +0700

    create variation method. with pointer and reference

--- main.cpp
+++ main.cpp
@@ -18,9 +18,45 @@ public:
     }
 
     // method tanpa parameter dan tanpa return
-    void tampilkanData()
+    void showData()
     {
         cout << "Nama saya adalah " << name << ", nim " << nim << " telah mengambil matkul " << matkul << " dan memperoleh nilai " << nilai << endl;
+    }
+
+    // method dengan parameter dan tanpa erturn
+
+    void update(const char* newName)
+    {
+        Mahasiswa::name = newName;
+    }
+
+    // method tanpa parameter dan dengan return
+    string getName ()
+    {
+        return Mahasiswa::name;
+    }
+
+    string getNim ()
+    {
+        return Mahasiswa::nim;
+    }
+
+    string getMatkul ()
+    {
+        return Mahasiswa::matkul;
+    }
+
+    int getNilai()
+    {
+        return nilai;
+    }
+
+
+    // method dengan parameter dan return
+
+    int bonusNilai(const int &tambahNilai)
+    {
+        return Mahasiswa::nilai + tambahNilai;
     }
 };
 
@@ -28,7 +64,15 @@ public:
 int main ()
 {
     Mahasiswa mahasiswa1 = Mahasiswa("shinta", "21120121140093", "DKP", 85);
-    mahasiswa1.tampilkanData();
+    mahasiswa1.showData();
+
+    mahasiswa1.update("nur");
+    mahasiswa1.showData();
+
+    string dataName = mahasiswa1.getName();
+    cout << "data nama = " << dataName << endl;
+    cout << "data nim = " << mahasiswa1.getNim() << endl;
+    cout << "Nilai akhir " << mahasiswa1.bonusNilai(10) << endl;
 
 return 0;
 }
